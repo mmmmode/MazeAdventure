35636ada99d4ddde26e794a69cad14165b7786a5
wenhuang4 <wenhuang4@iflytek.com>
2019-12-14 12:31:48 +0800

    [Subject]更新apk
    [Bug/Feature]
    
    [Ripple Effect]
    
    [Solution]
    
    [project]

---
+++ app/src/main/java/com/uestc/mode/mazeadventure/manager/AchieveMentManger.java
@@ -0,0 +1,12 @@
+package com.uestc.mode.mazeadventure.manager;
+
+/**
+ * @author mode
+ * date: 2019/12/14
+ */
+public class AchieveMentManger {
+    public static final String pass_teach = "pass_teach";//通过新手关
+    public static final String pong_wall_level1 = "pong_wall_level1";//撞墙100次
+    public static final String pong_wall_level2 = "pong_wall_level2";//撞墙100次
+    public static final String pong_wall_level3 = "pong_wall_level3";//撞墙100次
+}
